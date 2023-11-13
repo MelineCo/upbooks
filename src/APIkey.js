ec3c24a7f42c7efe3993d1de9bd5e79f05e2c01b
Emeline Courcelle <emelinecourcelle@MacBook-Air-de-Emeline.local>
2023-11-13 12:22:51 +0100

    Modify gitignore file

---
+++ src/APIkey.js
@@ -0,0 +1,1 @@
+export const APIkey = "AIzaSyBERZqi8XtOIIEAtjTKVXWrQgJxeawnG_8";
--- src/app/apiSlice.js
+++ src/app/apiSlice.js
@@ -1,11 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { APIkey } from "../APIkey";
 
 export const booksApi = createApi({
     reducerPath: 'booksApi',
     baseQuery: fetchBaseQuery({ baseUrl: 'https://www.googleapis.com/books/v1/' }),
     endpoints: (builder) => ({
         getBook: builder.query({
-            query: (title) => `volumes?q=${title}&key=AIzaSyBERZqi8XtOIIEAtjTKVXWrQgJxeawnG_8`,
+            query: (title) => `volumes?q=${title}&key=${APIkey}`,
         }),
     }),
 });
